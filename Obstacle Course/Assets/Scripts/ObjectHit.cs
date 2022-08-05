4f07b052b12933f579ba2e3ec0c9ead95288f276
onelessangel <69592470+onelessangel@users.noreply.github.com>
2022-08-05 14:47:03 +0300

    Fixed acorns, small bushes and tree log children collision

--- Obstacle Course/Assets/Scripts/ObjectHit.cs
+++ Obstacle Course/Assets/Scripts/ObjectHit.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+    MeshRenderer renderer;
+    MeshRenderer[] childrenRenderers;
+
+    
+
+    void Start() {
+        renderer = GetComponent<MeshRenderer>();
+        childrenRenderers = GetComponentsInChildren<MeshRenderer>();
+        // children = GetComponentsInChildren<Transform>(); 
+    }
+
     private void OnCollisionEnter(Collision other) {
         // Debug.Log("You've hit a wall!");
 
         try {
             if (other.gameObject.tag == "Player") {
-                GetComponent<MeshRenderer>().material.color = Color.red;
+                renderer.material.color = Color.red;
                 gameObject.tag = "Hit";
-            }            
+
+                foreach (MeshRenderer mr in childrenRenderers) {
+                    mr.material.color = Color.red;
+                }
+            } 
         } catch (Exception e) {
             Debug.Log(e);
         }
