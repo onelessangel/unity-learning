03f949aa96e4d8cf33b42f33888abd668b5ba086
onelessangel <69592470+onelessangel@users.noreply.github.com>
2022-08-17 12:18:32 +0300

    Added basic input binding

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust() {
+        if (Input.GetKey(KeyCode.Space)) {
+            Debug.Log("Pressed SPACE - Thrusting");
+        }
+    }
+
+    void ProcessRotation() {
+        if (Input.GetKey(KeyCode.A)) {
+            Debug.Log("rotate left");
+        } else if (Input.GetKey(KeyCode.D)) {
+            Debug.Log("rotate right");
+        }
     }
 }
