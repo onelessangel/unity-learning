48161a08f28ea6748165206c32da137b1675bec8
onelessangel <69592470+onelessangel@users.noreply.github.com>
2022-08-23 17:57:35 +0300

    Processed rotation and added basic SFX main thrusting effect

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float mainThrust = 1000f;
+    float mainThrust = 1000f;
+    float rotationThrust = 100f; 
     Rigidbody rb;
-    
 
     // Start is called before the first frame update
     void Start() {
@@ -28,9 +28,17 @@ public class Movement : MonoBehaviour
 
     void ProcessRotation() {
         if (Input.GetKey(KeyCode.A)) {
-            Debug.Log("rotate left");
+            // Debug.Log("rotate left");
+            ApplyRotation(rotationThrust);
         } else if (Input.GetKey(KeyCode.D)) {
-            Debug.Log("rotate right");
+            // Debug.Log("rotate right");
+            ApplyRotation(-rotationThrust);
         }
+    }
+
+    private void ApplyRotation(float rotationThisFrame) {
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // unfreezing rotation so the physics system can take over
     }
 }
