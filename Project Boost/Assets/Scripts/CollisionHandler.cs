9e690410beeb20ffab9fa5f83f4e7ef20f7c0586
onelessangel <69592470+onelessangel@users.noreply.github.com>
2022-08-30 13:29:01 +0300

    Added SuccessSequence and basic CrashSequence

--- Project Boost/Assets/Scripts/CollisionHandler.cs
+++ Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -3,22 +3,55 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 2f;
+
     void OnCollisionEnter(Collision other) {
        switch (other.gameObject.tag) {
         case "Friendly":
             Debug.Log("This thing is friendly");
             break;
         case "Finish":
-            Debug.Log("Congrats! You finished!");
+            StartSuccessSequence();
             break;
         default:
-            ReloadLevel();
+            StartCrashSequence();
             break;
         }
     }
 
-    private static void ReloadLevel() {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    private void StartSuccessSequence() {
+        GetComponent<Movement>().enabled = false;   // disabling player control after crashing
+        delayReload("LoadNextLevel");
+    }
+
+    private void StartCrashSequence() {
+        // TODO add SFX upon crash
+        // TODO add particle effect upon crash
+        GetComponent<Movement>().enabled = false;   // disabling player control after crashing
+        delayReload("ReloadLevel");
+    }
+
+    private void ReloadLevel() {
+        int currentSceneIndex = getCurrentSceneIndex();
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void LoadNextLevel() {
+        int currentSceneIndex = getCurrentSceneIndex();
+        int nextSceneIndex = ++currentSceneIndex;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private int getCurrentSceneIndex() {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void delayReload(string method) {
+        Invoke(method, reloadDelay);
     }
 }
