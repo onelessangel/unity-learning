91c916cdb2af3a5ed9f3b8cf486ed1df6ab603ff
onelessangel <69592470+onelessangel@users.noreply.github.com>
2022-09-14 11:29:49 +0300

    Added oscillation to obstacle in L1
    - used Range for movement factor
    - used Mathf.Sin for oscillation movement
    - used Mathf.Epsilon to fix NaN bug - division by 0

---
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startPosition;
+    [SerializeField] Vector3 movementVector;
+    [Range(0,1)] float movementFactor;
+    [SerializeField] float period = 5f;
+
+    // Start is called before the first frame update
+    void Start() {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (period <= Mathf.Epsilon) {
+            return;
+        }
+
+        float cycles = Time.time / period;              // continually growing over time
+    
+        const float tau = Mathf.PI * 2;                 // constant value of 6.283
+        float rawSignWave = Mathf.Sin(cycles * tau);    // going from -1 to 1
+
+        movementFactor = (rawSignWave + 1f) / 2;        // recalculated to go from 0 to 1 so it's cleaner
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startPosition + offset;
+    }
+}
